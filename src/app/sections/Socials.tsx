656d524e6e28a03e9458ea004d9a302469af2e22
sky46 <skyler@skylerma.com>
2024-09-26 21:17:45 -0400

    feat: make dogprints a little smaller for sponsors and socials

--- src/app/sections/Socials.tsx
+++ src/app/sections/Socials.tsx
@@ -8,9 +8,9 @@ const Socials = () => {
   return (
     <section id="socials" className="flex flex-col items-center text-center justify-center">
       <div className="flex justify-between items-center w-full py-8 mt-24 mb-8">
-        <img className="w-1/3" src="./images/sections/dogprints.png"></img>
+        <img className="w-1/4 md:w-1/3" src="./images/sections/dogprints.png"></img>
         <h2 className="text-4xl md:text-7xl font-display text-section-brown">socials</h2>
-        <img className="w-1/3" src="./images/sections/dogprints.png"></img>
+        <img className="w-1/4 md:w-1/3" src="./images/sections/dogprints.png"></img>
       </div>
       <div className="font-body md:text-2xl lg:text-3xl w-5/6 xl:w-3/4">
         <p className="text-nav-brown mb-10">
--- src/app/sections/Sponsors.tsx
+++ src/app/sections/Sponsors.tsx
@@ -5,9 +5,9 @@ const Sponsors = () => {
       className="flex flex-col items-center text-center justify-center"
     >
       <div className="flex justify-between items-center w-full py-8 mt-24 mb-8">
-        <img className="w-1/3" src="./images/sections/dogprints.png"></img>
+        <img className="w-1/4 md:w-1/3" src="./images/sections/dogprints.png"></img>
         <h2 className="text-4xl md:text-7xl font-display text-section-brown">sponsors</h2>
-        <img className="w-1/3" src="./images/sections/dogprints.png"></img>
+        <img className="w-1/4 md:w-1/3" src="./images/sections/dogprints.png"></img>
       </div>  
       <div className="font-body md:text-2xl lg:text-3xl text-nav-brown w-5/6 xl:w-3/4">
         <p className="mb-10">
